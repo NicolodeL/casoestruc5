cce621635b7cfe017058de51658b752cf25dfd5d
NlodeL <nlodelop@myuax.com>
2024-04-15 19:40:20 +0200

    añado codigo al main para poner a prueba la cadena contar Genes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import AnalisisGenomico.ConteoGenes;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String dna = "ATGCGTATGTTGATG"; // Puedes reemplazar esto con tu cadena de ADN
+        int count = ConteoGenes.contarGenes(dna);
+        System.out.println("Número de genes: " + count);
     }
 }
