177b5b422e767b8e68ecb8e0c368afcb80cd8936
NlodeL <nlodelop@myuax.com>
2024-04-16 19:30:13 +0200

    añado busqueda de palabras de forma lineal a la clase "BusquedaLineal"

---
+++ src/main/java/GestionInformacion/BusquedaLineal.java
@@ -0,0 +1,25 @@
+package GestionInformacion;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BusquedaLineal {
+
+    public void buscarPalabra(String rutaArchivo, String palabraBusqueda) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(rutaArchivo));
+        String linea;
+        int numLinea = 0;
+
+        while ((linea = reader.readLine()) != null) {
+            numLinea++;
+            String[] palabras = linea.split("\\s+");
+            for (int i = 0; i < palabras.length; i++) {
+                if (palabras[i].equals(palabraBusqueda)) {
+                    System.out.println("Palabra '" + palabraBusqueda + "' encontrada en la línea " + numLinea + ", palabra " + (i+1));
+                }
+            }
+        }
+        reader.close();
+    }
+}
