a9155f3c1cb339d695d8df05532df2c863b3c837
NlodeL <nlodelop@myuax.com>
2024-04-16 10:05:32 +0200

    añado las nuevas funciones al main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import AnalisisGenomico.CombinacionesGeneticas;
 import AnalisisGenomico.ConteoGenes;
+import HerramientaAnalisisNumerico.PontenciasyMaximos;
 
 import java.util.Scanner;
 
@@ -20,6 +21,18 @@ public class Main {
         int n = sc.nextInt();
         int[][] result = matrixMultiplication(fib(n), baseFibNumbers);
         System.out.println("Fib(" + n + ") = " + result[1][0]);
+
+        // Llamada al método calcularPotencia
+        double base = 2.0;
+        int exponente = 3;
+        double potencia = PontenciasyMaximos.calcularPotencia(base, exponente);
+        System.out.println("Potencia: " + potencia);
+
+        // Llamada al método encontrarMaximo
+        double[] datos = {1.2, 3.4, 0.8, 5.6, 4.3};
+        double maximo = PontenciasyMaximos.encontrarMaximo(datos);
+        System.out.println("Máximo: " + maximo);
+
         sc.close();
     }
 }
